0898b9d6c3772738885d5d5b9b5b1f218b3c9c32
smta028 <samtasingh028@gmail.com>
2023-05-20 23:05:02 +0530

    samta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Table from './Table';
+import { useState, useEffect } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`);
+                const datajson = await response.json();
+                setData(datajson)
+                console.log(data);
+                console.log(data);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Symbol</th>
+                    <th>Current Price</th>
+                    <th>Total Volume</th>
+                    <th>Price Change</th>
+                    <th>Market Cap</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(item => (
+                    <Table key={item.id} data={item} />
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+
 
 export default App;
+
